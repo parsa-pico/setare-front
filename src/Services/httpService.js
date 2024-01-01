5458eaaaab3e1f1af4951ddc2bb7f67f7f6d3db0
Parsa Foroozmand <parsaforoozmand@gmail.com>
2024-01-01 16:23:04 +0330

    #

--- src/Services/httpService.js
+++ src/Services/httpService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 axios.defaults.baseURL = process.env.REACT_APP_API_URL;
-axios.defaults.withCredentials = true;
+
 axios.interceptors.response.use(null, (error) => {
   const expectedError =
     error.response &&
